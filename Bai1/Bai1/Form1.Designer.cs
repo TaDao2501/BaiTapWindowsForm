05cc8f85473982f6961f1329b49a9a399d254ba5
TaDao2501 <91553171+TaDao2501@users.noreply.github.com>
2021-12-03 16:27:07 +0700

    Add files via upload

---
+++ Bai1/Bai1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Bai1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHoVaTen = new System.Windows.Forms.Label();
+            this.lblHoLot = new System.Windows.Forms.Label();
+            this.lblTen = new System.Windows.Forms.Label();
+            this.txtHoLot = new System.Windows.Forms.TextBox();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.btnHoLot = new System.Windows.Forms.Button();
+            this.btnTen = new System.Windows.Forms.Button();
+            this.btnHoVaTen = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHoVaTen
+            // 
+            this.lblHoVaTen.BackColor = System.Drawing.Color.Yellow;
+            this.lblHoVaTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoVaTen.Location = new System.Drawing.Point(64, 24);
+            this.lblHoVaTen.Name = "lblHoVaTen";
+            this.lblHoVaTen.Size = new System.Drawing.Size(233, 37);
+            this.lblHoVaTen.TabIndex = 0;
+            this.lblHoVaTen.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblHoVaTen.DoubleClick += new System.EventHandler(this.lblHoVaTen_DoubleClick);
+            // 
+            // lblHoLot
+            // 
+            this.lblHoLot.BackColor = System.Drawing.Color.Cyan;
+            this.lblHoLot.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoLot.Location = new System.Drawing.Point(29, 105);
+            this.lblHoLot.Name = "lblHoLot";
+            this.lblHoLot.Size = new System.Drawing.Size(100, 23);
+            this.lblHoLot.TabIndex = 1;
+            this.lblHoLot.Text = "Họ Lót";
+            // 
+            // lblTen
+            // 
+            this.lblTen.BackColor = System.Drawing.Color.Cyan;
+            this.lblTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTen.Location = new System.Drawing.Point(29, 147);
+            this.lblTen.Name = "lblTen";
+            this.lblTen.Size = new System.Drawing.Size(100, 23);
+            this.lblTen.TabIndex = 2;
+            this.lblTen.Text = "Tên";
+            // 
+            // txtHoLot
+            // 
+            this.txtHoLot.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHoLot.Location = new System.Drawing.Point(145, 105);
+            this.txtHoLot.Name = "txtHoLot";
+            this.txtHoLot.Size = new System.Drawing.Size(202, 22);
+            this.txtHoLot.TabIndex = 3;
+            this.txtHoLot.TextChanged += new System.EventHandler(this.txtHoLot_TextChanged);
+            // 
+            // txtTen
+            // 
+            this.txtTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTen.Location = new System.Drawing.Point(145, 148);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(202, 22);
+            this.txtTen.TabIndex = 4;
+            this.txtTen.TextChanged += new System.EventHandler(this.txtTen_TextChanged);
+            // 
+            // btnHoLot
+            // 
+            this.btnHoLot.Location = new System.Drawing.Point(32, 215);
+            this.btnHoLot.Name = "btnHoLot";
+            this.btnHoLot.Size = new System.Drawing.Size(89, 34);
+            this.btnHoLot.TabIndex = 5;
+            this.btnHoLot.Text = "&Họ Lót";
+            this.btnHoLot.UseVisualStyleBackColor = true;
+            this.btnHoLot.Click += new System.EventHandler(this.btnHoLot_Click);
+            // 
+            // btnTen
+            // 
+            this.btnTen.Location = new System.Drawing.Point(145, 215);
+            this.btnTen.Name = "btnTen";
+            this.btnTen.Size = new System.Drawing.Size(89, 34);
+            this.btnTen.TabIndex = 6;
+            this.btnTen.Text = "&Tên";
+            this.btnTen.UseVisualStyleBackColor = true;
+            this.btnTen.Click += new System.EventHandler(this.btnTen_Click);
+            // 
+            // btnHoVaTen
+            // 
+            this.btnHoVaTen.Location = new System.Drawing.Point(258, 215);
+            this.btnHoVaTen.Name = "btnHoVaTen";
+            this.btnHoVaTen.Size = new System.Drawing.Size(89, 34);
+            this.btnHoVaTen.TabIndex = 7;
+            this.btnHoVaTen.Text = "Họ &Và Tên";
+            this.btnHoVaTen.UseVisualStyleBackColor = true;
+            this.btnHoVaTen.Click += new System.EventHandler(this.btnHoVaTen_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(145, 276);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 34);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "&Kết Thúc";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 331);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnHoVaTen);
+            this.Controls.Add(this.btnTen);
+            this.Controls.Add(this.btnHoLot);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.txtHoLot);
+            this.Controls.Add(this.lblTen);
+            this.Controls.Add(this.lblHoLot);
+            this.Controls.Add(this.lblHoVaTen);
+            this.Name = "Form1";
+            this.Text = "Họ và tên";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHoVaTen;
+        private System.Windows.Forms.Label lblHoLot;
+        private System.Windows.Forms.Label lblTen;
+        private System.Windows.Forms.TextBox txtHoLot;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.Button btnHoLot;
+        private System.Windows.Forms.Button btnTen;
+        private System.Windows.Forms.Button btnHoVaTen;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Bai1/Bai1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Bai1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHoLot_Click(object sender, EventArgs e)
+        {
+            if (txtHoLot.Text != "")
+            {
+                lblHoLot.Text = txtHoLot.Text;
+            }
+            else
+            {
+                MessageBox.Show("Họ lót bị trống!");
+                txtHoLot.Focus();
+            }
+        }
+
+        private void btnTen_Click(object sender, EventArgs e)
+        {
+            if (txtTen.Text != "")
+            {
+                lblTen.Text = txtTen.Text;
+            }
+            else
+            {
+                MessageBox.Show("Tên bị trống!");
+                txtTen.Focus();
+            }
+        }
+
+        private void btnHoVaTen_Click(object sender, EventArgs e)
+        {
+            if (txtHoLot.Text != ""&&txtTen.Text!="")
+            {
+                lblHoVaTen.Text = txtHoLot.Text + " " + txtTen.Text;
+            }
+            else
+            {
+                MessageBox.Show("Họ lót và tên bị trống!");
+                txtHoLot.Focus();
+            }
+        }
+
+        private void lblHoVaTen_DoubleClick(object sender, EventArgs e)
+        {
+            lblHoVaTen.Text = "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtHoLot_TextChanged(object sender, EventArgs e)
+        {
+            lblHoLot.Text = txtHoLot.Text;
+        }
+
+        private void txtTen_TextChanged(object sender, EventArgs e)
+        {
+            lblTen.Text = txtTen.Text;
+        }
+    }
+}
